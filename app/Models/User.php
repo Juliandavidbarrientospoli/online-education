c0b1a91ca7b7d927146ff08de35f020b266ed814
Juliandavidbarrientospoli <julianjaramillo2011@gmail.com>
2024-11-07 13:08:26 -0500

    Añadiendo archivos iniciales al proyecto

---
+++ app/Models/User.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
+
+    /**
+     * Los atributos que son asignables en masa.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    /**
+     * Los atributos que deben ser ocultados para arrays.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * Los atributos que deben ser convertidos a tipos nativos.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Relación uno a muchos: Un usuario (administrador) puede crear muchos cursos.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function coursesCreated()
+    {
+        return $this->hasMany(Course::class, 'created_by');
+    }
+
+    /**
+     * Relación muchos a muchos: Un usuario puede estar inscrito en muchos cursos.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function coursesEnrolled()
+    {
+        return $this->belongsToMany(Course::class, 'enrollments')->withPivot('progress', 'current_video_id')->withTimestamps();
+    }
+
+    /**
+     * Relación uno a muchos: Un usuario puede tener muchos comentarios.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    /**
+     * Relación uno a muchos: Un usuario puede dar muchos likes.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
